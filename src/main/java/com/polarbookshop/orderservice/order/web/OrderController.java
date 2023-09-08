bf1dde41aa62fc867b30fea8a5ad59308e505a81
Tom89757 <2689980244@qq.com>
2023-09-08 11:20:58 +0800

    add data migration and audit

--- src/main/java/com/polarbookshop/orderservice/order/web/OrderController.java
+++ src/main/java/com/polarbookshop/orderservice/order/web/OrderController.java
@@ -4,6 +4,8 @@ import javax.validation.Valid;
 
 import com.polarbookshop.orderservice.order.domain.Order;
 import com.polarbookshop.orderservice.order.domain.OrderService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -17,20 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("orders")
 public class OrderController {
 
-	private final OrderService orderService;
+    private final OrderService orderService;
 
-	public OrderController(OrderService orderService) {
-		this.orderService = orderService;
-	}
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
 
-	@GetMapping
-	public Flux<Order> getAllOrders() {
-		return orderService.getAllOrders();
-	}
+    @GetMapping
+    public Flux<Order> getAllOrders(@AuthenticationPrincipal Jwt jwt) {
+        return orderService.getAllOrders(jwt.getSubject());
+    }
 
-	@PostMapping
-	public Mono<Order> submitOrder(@RequestBody @Valid OrderRequest orderRequest) {
-		return orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity());
-	}
+    @PostMapping
+    public Mono<Order> submitOrder(@RequestBody @Valid OrderRequest orderRequest) {
+        return orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity());
+    }
 
 }
